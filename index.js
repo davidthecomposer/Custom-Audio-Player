5bbe3aa3f8b8a6bd3eaebbfda9173cbd448e9077
David Campbell <davidthecomposer@gmail.com>
2020-01-23 09:23:20 -0700

    updated audio player class names and images

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const playerNumbers = [...audioPlayers].map((player) => player.classList[1]);
 
 class AudioPlayer {
   constructor(playerNumber) {
-    this.player = document.querySelector(`.${playerNumber}`);
-    this.buttons = this.player.querySelector('.buttons');
-    this.progressButton = this.player.querySelector('.progress-button');
-    this.progressBar = this.player.querySelector('progress');
-    this.audio = this.player.querySelector('audio');
-    this.currentPosition = this.player.querySelector('.current-position p');
-    this.timeRemaining = this.player.querySelector('.time-remaining p');
-    this.volume = this.player.querySelector('.volume');
-    this.volumeBar = this.player.querySelector('.volumeBar');
+    this.player = document.querySelector(`.audio-player.${playerNumber}`);
+    this.buttons = this.player.querySelector('.audio-buttons');
+    this.progressButton = this.player.querySelector('.audio-progress-button');
+    this.progressBar = this.player.querySelector('.audio-progress');
+    this.audio = this.player.querySelector('.audio-file');
+    this.currentPosition = this.player.querySelector('.audio-current-position p');
+    this.timeRemaining = this.player.querySelector('.audio-time-remaining p');
+    this.volume = this.player.querySelector('.audio-volume');
+    this.volumeBar = this.player.querySelector('.audio-volumeBar');
   }
 
   /* Gets time info from audio object and translates it to timeRemaining text*/
@@ -20,7 +20,7 @@ class AudioPlayer {
     let time = Math.floor(this.audio.duration.toFixed(0)) - Math.floor(this.audio.currentTime.toFixed(0));
     this.timeRemaining.innerHTML = Math.floor(time / 60) + ":" + (time < 10 ? `0${time}` : time % 60 < 10 ? `0${time % 60}` : time % 60 ? time % 60 : '00');
     if (this.audio.duration - this.audio.currentTime === 0) {
-      this.buttons.src = 'images/playbutton.svg';
+      this.buttons.src = 'images/audio-player/playbutton.svg';
     }
   }
 
@@ -44,8 +44,9 @@ class AudioPlayer {
 
   /* Handles the play/pause button logic*/
   buttonClickHandle(event) {
-    let play = 'images/playbutton.svg';
-    let pause = 'images/pausebutton.svg';
+    let play = 'images/audio-player/playbutton.svg';
+    let pause = 'images/audio-player/pausebutton.svg';
+    console.log('working')
     if (event.target.src.includes(play)) {
       event.target.src = pause;
       this.audio.play();
@@ -57,8 +58,8 @@ class AudioPlayer {
 
   /* handles the volume icons and controls*/
   muteVolume(event) {
-    let mute = 'images/volumeMute.svg';
-    let volumeIcon = 'images/volumeIcon.svg';
+    let mute = 'images/audio-player/volumeMute.svg';
+    let volumeIcon = 'images/audio-player/volumeIcon.svg';
 
     if (event.target.src.includes(volumeIcon)) {
       event.target.src = mute;
